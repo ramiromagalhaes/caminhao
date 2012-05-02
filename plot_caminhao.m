fb6df80c05b3b904046b1aaa91ac53d6e6e27bbb
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-05-01 23:56:59 -0300

    Formatei a função plot_caminhao para ficar mais apresentável. Alterei a função estaciona para que ela possa ser usada para plotar o caminhão, se assim for desejado. A função estaciona passou a usar a função set_phi(), para garantir que phi permanecerá dentro de seus limites. Precisei alterar caminhao.m para que ele usasse a nova versão da função estaciona adequadamente. Também baixei o erro tolerado de 5% para 4%, pois PARECE que isso tornou os resultados melhores (o caminhão consegue parar mais perto da parede). Por fim, e mais importante, ALTEREI CAMINHAO.FIS. Procurei melhorar as regras e mudar os parâmetros das funções de inclusão para tornar a capacidade do caminhão estacionar melhor quando ele estiver no centro ou em centro-esquerda, ou em centro-direita.

--- plot_caminhao.m
+++ plot_caminhao.m
@@ -1,22 +1,22 @@
-function plot_caminhao(x,y,phi, lc ,cc);
-% Desenha caminhao na tela
-%  x = posicao do caminhao na garagem
-%  y = posicao do caminhao na garagem
-%  phi = angulo do caminhao
-%  Adriano Cruz
-%  UFRJ
-%  Copyright 1999
+function plot_caminhao(x, y, phi, lc, cc)
+    % Desenha caminhao na tela
+    %  x = posicao do caminhao na garagem
+    %  y = posicao do caminhao na garagem
+    %  phi = angulo do caminhao
+    %  Adriano Cruz
+    %  UFRJ
+    %  Copyright 1999
 
-phirad=(phi*pi)/180;
-x2=x+0.5*lc*sin(phirad);
-y2=y-0.5*lc*cos(phirad);
-x3=x-0.5*lc*sin(phirad);
-y3=y+0.5*lc*cos(phirad);
-x1=x2-cc*cos(phirad);
-y1=y2-cc*sin(phirad);
-x4=x3-cc*cos(phirad);
-y4=y3-cc*sin(phirad);
-plot([x3 x4 x1 x2], [y3 y4 y1 y2]);
-plot([x2 x3], [y2 y3], '*:');
-%plot([x1 x2 x3 x4 x1], [y1 y2 y3 y4 y1]);
+    x2 = x + 0.5 * lc * sind(phi);
+    x3 = x - 0.5 * lc * sind(phi);
+    x1 = x2 - cc * cosd(phi);
+    x4 = x3 - cc * cosd(phi);
 
+    y2 = y - 0.5 * lc * cosd(phi);
+    y3 = y + 0.5 * lc * cosd(phi);
+    y1 = y2 - cc * sind(phi);
+    y4 = y3 - cc * sind(phi);
+
+    plot([x3 x4 x1 x2], [y3 y4 y1 y2]);
+    plot([x2 x3], [y2 y3], '*:');
+end
