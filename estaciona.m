f74fce97e5f68af5f943e4aeeaa21babc521060a
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-05-20 11:08:01 -0300

    Removendo funções inúteis.

--- estaciona.m
+++ estaciona.m
@@ -105,53 +105,20 @@ end
 
 
 function err = eval_err(val, expected)
-   %Avalia qual o erro percentual entre val e expected.
-   %O parâmetro val e o valor atual; expected e o valor esperado.
+    %Avalia qual o erro percentual entre val e expected.
+    %O parâmetro val e o valor atual; expected e o valor esperado.
     err = abs((val - expected) / expected);
 end
 
 function newphi = set_phi(p)
-%Calcula o valor de phi considerando seus limites máximo e mínimo.
-%Segundo a definição do problema, phi é um ângulo entre -90 e 270. Esta
-%função se certifica de que seu valor permanecerá dentro desses limites.
+    %Calcula o valor de phi considerando seus limites máximo e mínimo.
+    %Segundo a definição do problema, phi é um ângulo entre -90 e 270. Esta
+    %função se certifica de que seu valor permanecerá dentro desses limites.
     newphi = p;
     if (p > 270.0)
         newphi = p - 360.0;
     end
     if (p < -90.0)
         newphi = p + 360.0;
-    end
-end
-
-
-
-function val = distancia_canto_proximo(x, y, estacionamento)
-%Calcula a distancia do caminhão até o canto mais próximo do
-%estacionamento.
-    x_pow = 0;
-    if (x > (estacionamento(2) - estacionamento(1))/2)
-        x_pow = estacionamento(2) - x;
-    else
-        x_pow = x - estacionamento(1);
-    end
-    x_pow = x_pow^2;
-
-    y_pow = 0;
-    if (y > (estacionamento(4) - estacionamento(3))/2)
-        y_pow = estacionamento(4) - y;
-    else
-        y_pow = y - estacionamento(3);
-    end
-    y_pow = y_pow^2;
-
-    val = sqrt(x_pow + y_pow);
-end
-
-function atg = angulo_meta(x, y, phi, xmeta, ymeta)
-%Retorna o ângulo em graus da inclinação da reta que passa por (x,y) e
-%(xmeta,ymeta).
-    atg = atand((ymeta - y) / (xmeta - x));
-    if (atg < 0)
-        atg = atg + 180;
     end
 end
